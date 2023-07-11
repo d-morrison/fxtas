42560d96cb7a267bb7516058173056cecbb42b4c
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2023-07-11 18:13:38 -0400

    testing conda-env

--- R/compute_prob_correct.R
+++ R/compute_prob_correct.R
@@ -7,15 +7,25 @@
 #' @return
 #' @export
 #'
-compute_prob_correct = function(dataset, biomarkers, DataScores)
+compute_prob_correct = function(dataset, biomarkers, DataScores, max_prob = 1)
 {
 
-  # prob_correct = (dataset == 0) |> colMeans() # old version
+  # sapply(biomarkers,
+  #        function(x)
+  #        {
+  #          min(
+  #            max_prob,
+  #            mean(dataset[[x]] == DataScores[[x]][1], na.rm = TRUE))
+  #        })
+
+  prob_correct = (dataset == 0) |> colMeans() # old version
   dataset |>
     summarize(
       across(
         .cols = all_of(biomarkers),
-        .fn = ~ mean(.x == DataScores[1], na.rm = TRUE))
+        .fn = ~ min(
+          max_prob,
+          mean(.x == DataScores[1], na.rm = TRUE)))
     ) |>
     unlist()
 }
