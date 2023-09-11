84761c453fd853f6573decdd80721804035fcb16
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2023-09-11 13:57:41 -0400

    minor decomp

---
+++ R/compile_biomarker_group_list.R
@@ -0,0 +1,135 @@
+#' Title
+#'
+#' @return
+#' @export
+#'
+compile_biomarker_group_list = function()
+{
+  tremors = c(
+    "Head tremor" = 1,
+    "Intention tremor" = 1,
+    "Resting tremor" = 1,
+    "Postural tremor" = 1,
+    "Intermittent tremor" = 1
+    # "Any tremor" = 1
+  )
+
+  parkinsons_vars =
+    vars = grep("Parkinson", value = TRUE, names(gp34))
+
+  mri_vars = c(
+    "Cerebral Atrophy",
+    "Cerebellar Atrophy",
+    "Cerebral WM Hyperintensity",
+    "Cerebellar WM Hyperintensity",
+    "MCP-WM Hyperintensity",
+    "Pons-WM Hyperintensity",
+    "Sub-Insular WM Hyperintensity",
+    "Periventricular WM Hyperintensity",
+    "Splenium (CC)-WM Hyperintensity",
+    "Genu (CC)-WM Hyperintensity",
+    "Corpus Callosum-Thickness"
+  )
+
+  cancer_vars =  c(
+    "Thyroid Cancer",
+    "Skin Cancer",
+    "Melanoma",
+    "Prostate Cancer",
+    "Other Cancer"
+  )
+
+  ataxia = c(
+    "Ataxia",
+    "Ataxia: severity*"
+    # grep("Parkinson", value = TRUE, names(visit1))
+  )
+
+  scid_vars =
+    vars = c(
+      "Bipolar I Disorder (MD01), Lifetime",
+      "Bipolar II Disorder (MD02), Lifetime",
+      "Other Bipolar Disorder (MD03), Lifetime",
+      "Major Depressive Disorder (MD04), Lifetime",
+      "Mood Disorder Due to GMC (MD07), Lifetime",
+      "Substance-Induced Mood Dis. (MD08), Lifetime",
+      "Primary Psychotic Symptoms (PS01), Lifetime"
+    )
+
+  cantab_vars = c(
+    "SWM Between errors*",
+    # "SST Median correct RT on GO trials*",
+    "RVP A signal detection*",
+    # "OTS Problems solved on first choice*",
+    "PAL Total errors (adjusted)*",
+    "RTI Five-choice movement time*")
+
+  scores =
+    c(
+      "MMSE Total Score*",
+      "BDS-2 Total Score*")
+
+  thyroid_vars = c(
+    "Hypothyroid",
+    "Hyperthyroid",
+    "Thyroid problems",
+    "Lupus",
+    "Rheumatoid arthritis",
+    "Multiple Sclerosis: Workup",
+    "ANA positive",
+    "Sjogrens Syndrome",
+    "Raynauds Syndrome"
+    # "Pulmonary Fibrosis" # none
+    # "Immunological Notes"
+
+
+  )
+
+  scl90_vars =
+    grep(
+      value = TRUE,
+      names(gp34),
+      pattern = "^SCL90.*\\*$") |>
+    sort()
+
+  thyroid_vars = c(
+    "Hypothyroid",
+    "Hyperthyroid",
+    "Thyroid problems",
+    "Lupus",
+    "Rheumatoid arthritis",
+    "Multiple Sclerosis: Workup",
+    "ANA positive",
+    "Sjogrens Syndrome",
+    "Raynauds Syndrome",
+    "Pulmonary Fibrosis"
+    # "Immunological Notes"
+
+
+  )
+  kinesia_vars = c(
+    "Left: Rest Tremor",
+    "Left: Postural Tremor",
+    "Left: Kinetic Tremor",
+    "Right: Rest Tremor",
+    "Right: Postural Tremor",
+    "Right: Kinetic Tremor"
+  )
+
+  biomarker_group_list =
+    list(
+      tremors = names(tremors),
+      ataxia = ataxia,
+      stage = "FXTAS Stage (0-5)*",
+      parkinsons = parkinsons_vars,
+      cancer = cancer_vars,
+      mri = mri_vars,
+      scores = scores,
+      scid = scid_vars,
+      scl90 = scl90_vars,
+      cantab = cantab_vars,
+      thyroid = thyroid_vars,
+      kinesia = kinesia_vars
+    )
+
+}
