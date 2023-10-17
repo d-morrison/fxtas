6bf702e28fc4f609f89dadd383f28ae864dac5b8
Matthew Ponzini <mdponzini@ucdavis.edu>
2023-10-17 16:08:21 -0700

    apoe.R: added code to handle inconsistent order of ApoE (e.g. E3/E4 is the same as E4/E3).
    update_apoe.R: inserted ApoE only entered for 1st obs of an ID, backfill. If updated NA, fill with previous data.
    
    exploration: added extra table to show ApoE, ApoE (backfilled), and ApoE (updated) for those with previously inconsistent ApoE values.

--- R/update_apoe.R
+++ R/update_apoe.R
@@ -1,9 +1,9 @@
 # update ApoE and CGG with data from Flora #
-update_apoe <- function(data){
+update_apoe <- function(dataset){
   # store ApoE levels from GP3/4 data
-  apoe_levels <- levels(data$`ApoE (backfilled)`)
+  apoe_levels <- levels(dataset$`ApoE (backfilled)`)
 
-  # convert ApoE corrections to factors with consistent levels
+  # convert ApoE corrections to factor with consistent levels
   apoe <- apoe |>
     mutate(
       `ApoE (backfilled)` = factor(`ApoE (backfilled)`,
@@ -12,10 +12,27 @@ update_apoe <- function(data){
 
   # replace ApoE values with ApoE corrections
   # https://stackoverflow.com/questions/40177132/replace-values-from-another-dataframe-by-ids
-  data$`ApoE (updated)` <- data$`ApoE (backfilled)`
-  data$`ApoE (updated)`[match(apoe$`FXS ID`, data$`FXS ID`)] <- apoe$`ApoE (backfilled)`
 
-  # relocate ApoE (updated) to after ApoE (backfilled) and return
-  data |>
+  # create empty ApoE updated with factor levels
+  dataset$`ApoE (updated)` <- factor(NA, levels = apoe_levels)
+  # fill in updated ApoE by ID
+  dataset$`ApoE (updated)`[match(apoe$`FXS ID`, dataset$`FXS ID`)] <- apoe$`ApoE (backfilled)`
+
+  # only inserts updated ApoE value into the first obs of a given ID, backfill
+  dataset |>
+    group_by(`FXS ID`) |>
+    tidyr::fill(
+      `ApoE (updated)`,
+      .direction = "downup") |>
+    # if ApoE (updated) is missing, fill with ApoE (backfilled)
+    mutate(
+      `ApoE (updated)` = dplyr::if_else(
+        !is.na(`ApoE (updated)`),
+        `ApoE (updated)`,
+        `ApoE (backfilled)`
+      )
+    ) |>
+    ungroup() |>
+    # relocate ApoE (updated) to after ApoE (backfilled) and return
     relocate(`ApoE (updated)`, .after = `ApoE (backfilled)`)
 }
--- data-raw/apoe.R
+++ data-raw/apoe.R
@@ -3,6 +3,19 @@ apoe <- readxl::read_xlsx(
   "inst/extdata/GP3 & GP4 -ApoE_ GP_Missing Data Table- 10-02-2023-mdp.xlsx"
 )
 
+apoe <- apoe |>
+  # order of ApoE doesn't matter (e.g. E3/E4 is same as E4/E3). make consistent
+  mutate(
+    `ApoE (backfilled)` =
+      `ApoE (backfilled)` |>
+      strsplit(", ") |>
+      sapply(sort) |>
+      sapply(paste, collapse = ", ") |>
+      na_if(""),
+
+    `ApoE (backfilled)` = factor(`ApoE (backfilled)`,
+                                 levels = sort(unique(`ApoE (backfilled)`)))
+  )
 
 apoe = tibble(apoe)
 usethis::use_data(apoe, overwrite = TRUE)
