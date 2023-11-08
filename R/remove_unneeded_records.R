c6111bc61608513418ac690bdbbd9dad43d158c7
Matthew Ponzini <mdponzini@ucdavis.edu>
2023-11-08 10:12:17 -0800

    trax contained record with id 999999-999, remove this record.
    added trax missing levels

--- R/remove_unneeded_records.R
+++ R/remove_unneeded_records.R
@@ -5,6 +5,7 @@ remove_unneeded_records = function(dataset)
       # duplicate of GP4 visit 1; note from Ellery Santos, 2022-12-19, in word doc:
       not(`FXS ID` == "100429-700" & `Event Name` == "GP3 - Visit 1"),
       # note from Ellery Santos, 2022-12-19, in word doc:
-      `FXS ID` != "500011-190"
+      `FXS ID` != "500011-190",
+      `FXS ID` != "999999-999"
     )
 }
