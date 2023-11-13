33e283e14c831d8a135d1ae079b9f111d5cf994e
Matthew Ponzini <mdponzini@ucdavis.edu>
2023-11-13 12:25:49 -0800

    removed erroneous missing labels that belonged in another script.

--- R/add_labels_to_missing_codes.R
+++ R/add_labels_to_missing_codes.R
@@ -4,11 +4,8 @@ add_labels_to_missing_codes = function(x)
     forcats::fct_recode(
       "Asked by clinician, but no answer from subject (99)" = "99" ,
       "Question not asked at time of data entry; check records (777)" = "777",
-      "Question not asked at time of data entry; check records" = "777",
       "NA (888)" = "888",
-      "NA" = "888",
-      "No Response (999)" = "999",
-      "No Response" = "999"
+      "No Response (999)" = "999"
     ) |>
     # tidyr::replace_na("Missing (empty in RedCap)") |> # doesn't work with factors
     forcats::fct_na_value_to_level(level = "Missing (empty in RedCap)") |>
