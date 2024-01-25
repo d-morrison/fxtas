3fbd94f8c8d76948004a583756dbdcdf382d8215
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2024-01-25 12:59:01 -0500

    cleanup

--- R/graph_CGG_repeats.R
+++ R/graph_CGG_repeats.R
@@ -1,35 +1,51 @@
 #' Barplot of CGG repeats
 #'
 #' @param data a [data.frame] containing a [numeric] or [integer] column named `CGG`
-#'
+#' @inheritParams ggplot2::facet_grid
 #' @return a [ggplot2::ggplot] object
 #' @export
 #'
 #' @examples
 #' visit1 |> graph_CGG_repeats()
-graph_CGG_repeats = function(data)
+#' visit1 |> graph_CGG_repeats(cols = vars(Gender))
+graph_CGG_repeats = function(
+    data,
+    rows = NULL,
+    cols = NULL
+      )
 {
-  data |>
+  plot1 =
+    data |>
     ggplot(aes(x = .data$CGG)) +
     geom_bar(alpha = .5) +
     geom_vline(
-      aes(
-        xintercept = 55,
-        col =
-          "55 CGG repeats (control vs case)" #|>
-        # factor(
-        #   levels = c("55 CGG repeats (control vs case)",
-        #              "200 CGG repeats (pre- vs full- mutation)"))
-        )
+      data = tibble(
+        x = c(55, 200),
+        col = c(
+          "55 CGG repeats (control vs case)",
+          "200 CGG repeats (pre- vs full- mutation)") |>
+          factor() |>
+          relevel(ref = "55 CGG repeats (control vs case)")
+      ),
+      aes(xintercept = x, col = col)
       ) +
-    geom_vline(
-      aes(
-        xintercept = 200,
-        col = "200 CGG repeats (pre- vs full- mutation)")) +
+    # geom_vline(
+    #   aes(
+    #     xintercept = 200,
+    #     col = "200 CGG repeats (pre- vs full- mutation)")) +
     theme_bw() +
     theme(legend.position = "bottom") +
     labs(colour = "") +
     ylab("# study participants") +
     scale_x_log10() +
     xlab("# CGG repeats (logarithmic spacing)")
+
+  if(!is.null(rows) || !is.null(cols))
+  {
+    plot1 =
+      plot1 +
+      facet_grid(rows = rows, cols = cols)
+  }
+
+  return(plot1)
 }
