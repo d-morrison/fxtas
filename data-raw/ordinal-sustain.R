349f645dc7aab0c73885cb606dbc7ade98bf617b
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2024-05-15 11:10:23 -0700

    switch to run-and-save-OSA()

--- data-raw/ordinal-sustain.R
+++ data-raw/ordinal-sustain.R
@@ -150,7 +150,7 @@ sustain_output = run_and_save_OSA(
 #| message: false
 #| label: model-males
 #| include: false
-sustain_output_males = run_OSA(
+sustain_output_males = run_and_save_OSA(
   biomarker_levels = biomarker_levels,
   prob_correct = prob_correct,
   patient_data = patient_data |> filter(Gender == "Male"),
@@ -169,7 +169,7 @@ sustain_output_males = run_OSA(
 #| message: false
 #| label: model-females
 #| include: false
-sustain_output_females = run_OSA(
+sustain_output_females = run_and_save_OSA(
   biomarker_levels = biomarker_levels,
   prob_correct = prob_correct,
   patient_data = patient_data |> filter(Gender == "Female"),
@@ -189,7 +189,7 @@ sustain_output_females = run_OSA(
 #| message: false
 #| label: "cgg_over_100"
 #| include: false
-sustain_output_cgg100plus = run_OSA(
+sustain_output_cgg100plus = run_and_save_OSA(
   biomarker_levels = biomarker_levels,
   prob_correct = prob_correct,
   patient_data = patient_data |> filter(`CGG` >= 100),
@@ -208,7 +208,7 @@ sustain_output_cgg100plus = run_OSA(
 #| message: false
 #| label: "cgg_under_100"
 #| include: false
-sustain_output_cgg100minus = run_OSA(
+sustain_output_cgg100minus = run_and_save_OSA(
   biomarker_levels = biomarker_levels,
   prob_correct = prob_correct,
   patient_data = patient_data |> filter(`CGG` < 100),
@@ -225,7 +225,7 @@ sustain_output_cgg100minus = run_OSA(
 #| message: false
 #| label: "cgg_over_100 & Male"
 #| include: false
-sustain_output_cgg100plus_males = run_OSA(
+sustain_output_cgg100plus_males = run_and_save_OSA(
   biomarker_levels = biomarker_levels,
   prob_correct = prob_correct,
   patient_data = patient_data |>
@@ -247,7 +247,7 @@ sustain_output_cgg100plus_males = run_OSA(
 #| message: false
 #| label: "cgg_under_100 & Male"
 #| include: false
-sustain_output_cgg100minus_males = run_OSA(
+sustain_output_cgg100minus_males = run_and_save_OSA(
   biomarker_levels = biomarker_levels,
   prob_correct = prob_correct,
   patient_data = patient_data |>
@@ -268,7 +268,7 @@ sustain_output_cgg100minus_males = run_OSA(
 #| message: false
 #| label: "cgg_over_100 & Female"
 #| include: false
-sustain_output_cgg100plus_females = run_OSA(
+sustain_output_cgg100plus_females = run_and_save_OSA(
   biomarker_levels = biomarker_levels,
   prob_correct = prob_correct,
   patient_data = patient_data |>
@@ -291,7 +291,7 @@ sustain_output_cgg100plus_females = run_OSA(
 #| message: false
 #| label: "cgg_under_100 & Female"
 #| include: false
-sustain_output_cgg100minus_females = run_OSA(
+sustain_output_cgg100minus_females = run_and_save_OSA(
   biomarker_levels = biomarker_levels,
   prob_correct = prob_correct,
   patient_data = patient_data |>
