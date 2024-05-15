0611a8da0a12ccce7e421d95e258789e78a6b10d
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2024-05-15 11:13:58 -0700

    fix

--- R/run_and_save_OSA.R
+++ R/run_and_save_OSA.R
@@ -21,22 +21,21 @@ run_and_save_OSA = function(
   if(file.exists(rds_path) && !rerun)
   {
     if(verbose) cli::cli_inform("Found RDS file for {rds_filebase}; loading...")
-    sus_output = readRDS(rds_path)
-    testthat::expect_equal(dim(sus_output$samples_sequence)[1], N_S_max)
+    osa_output = readRDS(rds_path)
+    testthat::expect_equal(dim(osa_output$samples_sequence)[1], N_S_max)
   } else
   {
     if(verbose) cli::cli_inform("RDS file for {rds_filebase} not found; running OSA.")
-    sus_output = run_OSA(
-      biomarker_levels = biomarker_levels,
+    osa_output = run_OSA(
       dataset_name = dataset_name,
       output_folder = output_folder,
       N_S_max = N_S_max,
       verbose = verbose,
       ...
     )
-    sus_output |> saveRDS(rds_path)
+    osa_output |> saveRDS(rds_path)
   }
 
-  return(sus_output)
+  return(osa_output)
 
 }
