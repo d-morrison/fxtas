21bfac59a99990b667926a733844e4cbb3014d82
Matthew Ponzini <mdponzini@ucdavis.edu>
2024-06-07 15:44:56 -0700

    updated documentation

--- R/table_subtype_by_demographics.R
+++ R/table_subtype_by_demographics.R
@@ -3,35 +3,10 @@
 #' @param patient_data
 #' @param subtype_and_stage_table
 #'
-#' @returns A `"tableby"` object (extends [arsenal::arsenal_table]; see [arsenal::tableby])
+#' @returns A [gtsummary::tbl_summary]
 #' @export
 #'
-# table_subtype_by_demographics = function(
-#     patient_data,
-#     subtype_and_stage_table
-#     )
-# {
-#
-#   patient_data2 =
-#     patient_data |>
-#     bind_cols(subtype_and_stage_table)
-#
-#   patient_data2 |>
-#     # filter(ml_subtype != "Type 0") |>
-#     droplevels() |>
-#     tableby(
-#       ml_subtype ~
-#         `FX3*` +
-#         Gender +
-#         `Primary Race` +
-#         `Primary Ethnicity`,
-#       cat.stats = "countrowpct",
-#       data = _) |>
-#     summary(pfootnote=TRUE)
-# }
 
-
-# use gtsummary instead
 table_subtype_by_demographics = function(
     patient_data,
     subtype_and_stage_table
