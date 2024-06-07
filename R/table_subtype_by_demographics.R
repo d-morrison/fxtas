655c7398c2ec89c5d0109ea81b1a420e21971eac
Matthew Ponzini <mdponzini@ucdavis.edu>
2024-06-07 15:37:27 -0700

    changed table_subtype_by_demographics to use gtsummary instead of tableby.
    set seed for simulated fisher's exact test in results-latent-subgroups.qmd
    
    fixed inline code typo on line 118.
    
    converted optimal subtypes from hard coded to inline code. added text stating we used 6 subtypes for clinical reasons.

--- R/table_subtype_by_demographics.R
+++ R/table_subtype_by_demographics.R
@@ -6,10 +6,36 @@
 #' @returns A `"tableby"` object (extends [arsenal::arsenal_table]; see [arsenal::tableby])
 #' @export
 #'
+# table_subtype_by_demographics = function(
+#     patient_data,
+#     subtype_and_stage_table
+#     )
+# {
+#
+#   patient_data2 =
+#     patient_data |>
+#     bind_cols(subtype_and_stage_table)
+#
+#   patient_data2 |>
+#     # filter(ml_subtype != "Type 0") |>
+#     droplevels() |>
+#     tableby(
+#       ml_subtype ~
+#         `FX3*` +
+#         Gender +
+#         `Primary Race` +
+#         `Primary Ethnicity`,
+#       cat.stats = "countrowpct",
+#       data = _) |>
+#     summary(pfootnote=TRUE)
+# }
+
+
+# use gtsummary instead
 table_subtype_by_demographics = function(
     patient_data,
     subtype_and_stage_table
-    )
+)
 {
 
   patient_data2 =
@@ -19,13 +45,18 @@ table_subtype_by_demographics = function(
   patient_data2 |>
     # filter(ml_subtype != "Type 0") |>
     droplevels() |>
-    tableby(
-      ml_subtype ~
-        `FX3*` +
-        Gender +
-        `Primary Race` +
-        `Primary Ethnicity`,
-      cat.stats = "countrowpct",
-      data = _) |>
-    summary(pfootnote=TRUE)
+    dplyr::select(ml_subtype, `FX3*`, Gender, `Primary Race/Ethnicity`) |>
+    gtsummary::tbl_summary(
+      by = ml_subtype,
+      percent = "row",
+      missing = "no" # do not show missing
+      # missing_text = "Missing"
+    ) |>
+
+    gtsummary::add_p(
+      pvalue_fun = function(x) gtsummary::style_number(x, digits = 3),
+      test.args = c(`FX3*`, `Primary Race/Ethnicity`) ~ list(simulate.p.value = TRUE)
+    ) |>
+    gtsummary::add_overall() |>
+    gtsummary::modify_footnote(gtsummary::all_stat_cols() ~ "n (row %)")
 }
