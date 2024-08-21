16bad0eb94a337b519c1ca003f134da22d0c6a4b
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2024-08-21 00:10:04 -0700

    more cleanup

--- R/simplify_biomarker_names.R
+++ R/simplify_biomarker_names.R
@@ -5,7 +5,8 @@ simplify_biomarker_names = function(dataset, cols = "event name")
       .cols = all_of(cols),
       .fns = ~ .x |>
         simplify_pf_names() |>
-        update_any_autoimmune_name()
+        update_any_autoimmune_name() |>
+        remove_CC()
     ))
 }
 
@@ -25,3 +26,14 @@ update_any_autoimmune_name = function(names)
     stringr::str_replace(pattern = ": Some autoimmune recorded", replacement = "")
 }
 
+remove_CC = function(names)
+{
+  names |>
+    stringr::str_replace(pattern = "(CC)-", replacement = "")
+}
+
+numeric_five = function(names)
+{
+  names |>
+    stringr::str_replace(pattern = "Five-choice", replacement = "5-choice")
+}
