fd8f05445d7138b1e1961686dd485390af4b97e8
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2024-10-03 15:18:22 -0700

    in progress

--- R/make_demographics_table.R
+++ R/make_demographics_table.R
@@ -17,16 +17,16 @@ make_demographics_table = function(data)
     # "Primary Race",
     "FXTAS Stage (0-5)*",
     "CGG"
-    # "ApoE"
-  )
+    # "ApoE")
+    )
 
-  # create table using gtsummary with p-value for sex difference
-  tbl_stat <- gtsummary::tbl_strata(
-    data = data |>
-      dplyr::select(all_of(vars), Gender, `FX*`),
-    strata = c("Gender"),
-    .tbl_fun = ~ .x %>%
-      gtsummary::tbl_summary(
+    data_to_use = data |>
+      dplyr::select(all_of(vars), Gender, `FX*`)
+    # create table using gtsummary with p-value for sex difference
+
+    table_function = function(data) {
+      data |>
+        gtsummary::tbl_summary(
         by = `FX*`,
         type = gtsummary::all_continuous() ~ "continuous2",
         statistic = list(
@@ -34,43 +34,55 @@ make_demographics_table = function(data)
             c("{mean} ({sd})", "{median} [{min}, {max}]")
         ),
         # round mean to 1 digit, SD to 2 digits, Median and Range to 0
-        digits = list(
-          `Age at visit` ~ c(1, 2, 0, 0, 0),
-          CGG ~ c(1, 2, 0, 0, 0)),
+        digits = list(`Age at visit` ~ c(1, 2, 0, 0, 0), CGG ~ c(1, 2, 0, 0, 0)),
         missing_text = "Missing"
       )
-  )
+    }
 
-  tbl_pval <- data |>
-    dplyr::select(all_of(vars), Gender) |>
-    gtsummary::tbl_summary(by = Gender, missing_text = "Missing") |>
-    gtsummary::add_p(
-      pvalue_fun = function(x)
-        gtsummary::style_number(x, digits = 3)
-    ) |>
-    gtsummary::modify_column_hide(columns = c(stat_1, stat_2)) |>
-    gtsummary::separate_p_footnotes()
+    tbl_stat <-
+      gtsummary::tbl_strata(
+        data = data_to_use,
+        strata = c("Gender"),
+        .tbl_fun = ~ .x %>%
+          gtsummary::tbl_summary(
+            by = `FX*`,
+            type = gtsummary::all_continuous() ~ "continuous2",
+            statistic = list(
+              gtsummary::all_continuous() ~
+                c("{mean} ({sd})", "{median} [{min}, {max}]")
+            ),
+            # round mean to 1 digit, SD to 2 digits, Median and Range to 0
+            digits = list(`Age at visit` ~ c(1, 2, 0, 0, 0), CGG ~ c(1, 2, 0, 0, 0)),
+            missing_text = "Missing"
+          )
+      )
 
-  gtsummary::tbl_merge(list(tbl_stat, tbl_pval), tab_spanner = FALSE) |>
-    gtsummary::as_flex_table() |>
-    # update upper header label: '' to 'M vs. F'
-    flextable::compose(
-      part = "header",
-      i = 1,
-      j = "p.value_2",
-      value = flextable::as_paragraph(
-        "M vs. F"
-      )
-    ) |>
-    # update lower header label: 'p-value' to 'p-value*'
-    flextable::compose(
-      part = "header",
-      i = 2,
-      j = "p.value_2",
-      value = flextable::as_paragraph(
-        "p-value", flextable::as_sup("*")
-      )
-    ) |>
-    # reset p-value header column to bold font
-    flextable::bold(part = "header", j = 6)
-}
+    tbl_pval <- data |>
+      dplyr::select(all_of(vars), Gender) |>
+      gtsummary::tbl_summary(by = Gender, missing_text = "Missing") |>
+      gtsummary::add_p(
+        pvalue_fun = function(x)
+          gtsummary::style_number(x, digits = 3)
+      ) |>
+      gtsummary::modify_column_hide(columns = c(stat_1, stat_2)) |>
+      gtsummary::separate_p_footnotes()
+
+    gtsummary::tbl_merge(list(tbl_stat, tbl_pval), tab_spanner = FALSE) |>
+      gtsummary::as_flex_table() |>
+      # update upper header label: '' to 'M vs. F'
+      flextable::compose(
+        part = "header",
+        i = 1,
+        j = "p.value_2",
+        value = flextable::as_paragraph("M vs. F")
+      ) |>
+      # update lower header label: 'p-value' to 'p-value*'
+      flextable::compose(
+        part = "header",
+        i = 2,
+        j = "p.value_2",
+        value = flextable::as_paragraph("p-value", flextable::as_sup("*"))
+      ) |>
+      # reset p-value header column to bold font
+      flextable::bold(part = "header", j = 6)
+    }
