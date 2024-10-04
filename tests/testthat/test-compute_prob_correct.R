d9253122be70e03f8f82cfc4d980dfb70f2a5b37
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2024-10-03 22:44:38 -0700

    about to extend `compute_prob_correct()`

---
+++ tests/testthat/test-compute_prob_correct.R
@@ -0,0 +1,29 @@
+test_that("compute_prob_correct() produces consistent results", {
+
+  full_data = trax_gp34_v1
+
+  v1_usable = full_data |> filter(CGG < 200)
+
+  biomarker_groups = compile_biomarker_groups_table()
+
+  biomarker_varnames =
+    biomarker_groups |>
+    pull("biomarker")
+
+  biomarker_levels =
+    v1_usable |>
+    dplyr::select(all_of(biomarker_varnames)) |>
+    lapply(F = levels)
+
+  control_data =
+    df |>
+    filter(`FX*` == "CGG <55") |>
+    select(all_of(biomarker_varnames))
+
+  control_data |>
+    compute_prob_correct(
+      max_prob = .95,
+      biomarker_levels = biomarker_levels) |>
+    expect_snapshot_value(style = "deparse")
+
+})
