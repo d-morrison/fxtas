447987dd814e58bf2dba734cf10b4f5f8b7380e6
Matthew Ponzini <mdponzini@ucdavis.edu>
2024-10-17 17:24:38 -0700

    updated table to show column percentages and missing values, and include numeric CGG summaries.
    **need to update modified footnote now that we have mean (SD) as well**

--- R/table_subtype_by_demographics.R
+++ R/table_subtype_by_demographics.R
@@ -17,21 +17,35 @@ table_subtype_by_demographics = function(
     patient_data |>
     bind_cols(subtype_and_stage_table)
 
+  simulated_fisher <- function(data, variable, by, ...){
+    data <- data[c(variable, by)]
+    stats::fisher.test(
+      data[[variable]], as.factor(data[[by]]), simulate.p.value = TRUE, B = 10000,
+      conf.level = 0.95
+    ) |>
+      broom::tidy()
+  }
+
   patient_data2 |>
     filter(ml_subtype != "Type 0") |>
     droplevels() |>
-    dplyr::select(ml_subtype, `FX3*`, Gender, `Primary Race/Ethnicity`) |>
+    dplyr::select(ml_subtype, CGG, `FX3*`, Gender, `Primary Race/Ethnicity`) |>
     gtsummary::tbl_summary(
       by = ml_subtype,
-      percent = "row",
+      percent = "column", # revert back to column percentages
+      statistic = list(
+        gtsummary::all_continuous() ~ "{mean} ({sd})"
+      ),
       missing = "no" # do not show missing
       # missing_text = "Missing"
     ) |>
 
     gtsummary::add_p(
       pvalue_fun = function(x) gtsummary::style_number(x, digits = 3),
-      test.args = c(`FX3*`, `Primary Race/Ethnicity`) ~ list(simulate.p.value = TRUE)
+      test = list(
+        `Primary Race/Ethnicity` = "simulated_fisher"
+      )
     ) |>
-    gtsummary::add_overall() |>
-    gtsummary::modify_footnote(gtsummary::all_stat_cols() ~ "n (row %)")
+    gtsummary::add_overall()
+    # gtsummary::modify_footnote(gtsummary::all_stat_cols() ~ "n (row %)")
 }
