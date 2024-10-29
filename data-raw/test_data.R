d0a3dc8cd428eea26901c125ed481d7ecba8e480
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2024-10-29 16:09:41 -0700

    add test data

---
+++ data-raw/test_data.R
@@ -0,0 +1,26 @@
+test_data =
+  trax_gp34_all |>
+  select(-`Study ID Number`) |>
+  mutate(
+    `FXS ID` =
+      `FXS ID` |>
+      as.factor() |>
+      as.numeric() |>
+      as.character()
+  )
+
+for (i in colnames(test_data))
+{
+
+  test_data[,i] = test_data[,i] |> sample(replace = FALSE)
+
+}
+
+usethis::use_data(test_data, overwrite = TRUE)
+
+devtools::load_all()
+
+test_data_v1 = test_data |>
+  get_visit1()
+
+test_data_v1 |> usethis::use_data(overwrite = TRUE)
