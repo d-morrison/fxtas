012b976c5abca5e7c4f6e393d263fb841a23dc39
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2024-10-31 11:58:29 -0700

    spell check and documentation

--- R/table_subtype_by_demographics.R
+++ R/table_subtype_by_demographics.R
@@ -8,7 +8,7 @@
 #' @examples
 #' patient_data = test_data_v1 |> filter(CGG >= 55, CGG < 200)
 #' table = test_subtype_and_stage_table
-#' table_subtype_by_demographics(#'  patient_data, table)
+#' table_subtype_by_demographics(patient_data, table)
 
 table_subtype_by_demographics = function(
     patient_data,
