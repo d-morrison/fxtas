924642f1fe86588c02d4e9238e5ab2284be533fb
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2024-11-01 00:30:22 -0700

    sort of works

--- R/make_demographics_table.R
+++ R/make_demographics_table.R
@@ -6,6 +6,7 @@
 #' @export
 #'
 #' @examples
+#' test_data_v1 |> make_demographics_table()
 make_demographics_table = function(data)
 {
   vars = c(# "Study",
@@ -18,15 +19,15 @@ make_demographics_table = function(data)
     "FXTAS Stage (0-5)*",
     "CGG"
     # "ApoE")
-    )
+  )
 
-    data_to_use = data |>
-      dplyr::select(all_of(vars), Gender, `FX*`)
-    # create table using gtsummary with p-value for sex difference
+  data_to_use = data |>
+    dplyr::select(all_of(vars), Gender, `FX*`)
+  # create table using gtsummary with p-value for sex difference
 
-    table_function = function(data) {
-      data |>
-        gtsummary::tbl_summary(
+  table_function = function(data) {
+    data |>
+      gtsummary::tbl_summary(
         by = `FX*`,
         type = gtsummary::all_continuous() ~ "continuous2",
         statistic = list(
@@ -37,38 +38,52 @@ make_demographics_table = function(data)
         digits = list(`Age at visit` ~ c(1, 2, 0, 0, 0), CGG ~ c(1, 2, 0, 0, 0)),
         missing_text = "Missing"
       )
-    }
+  }
 
-    tbl_stat <-
-      gtsummary::tbl_strata(
-        data = data_to_use,
-        strata = c("Gender"),
-        .tbl_fun = ~ .x %>%
-          gtsummary::tbl_summary(
-            by = `FX*`,
-            type = gtsummary::all_continuous() ~ "continuous2",
-            statistic = list(
-              gtsummary::all_continuous() ~
-                c("{mean} ({sd})", "{median} [{min}, {max}]")
-            ),
-            # round mean to 1 digit, SD to 2 digits, Median and Range to 0
-            digits = list(`Age at visit` ~ c(1, 2, 0, 0, 0), CGG ~ c(1, 2, 0, 0, 0)),
-            missing_text = "Missing"
-          )
-      )
+  tbl_stat <-
+    gtsummary::tbl_strata(
+      data = data_to_use,
+      strata = c("Gender"),
+      .tbl_fun = ~ .x %>%
+        gtsummary::tbl_summary(
+          by = `FX*`,
+          type = gtsummary::all_continuous() ~ "continuous2",
+          statistic = list(
+            gtsummary::all_continuous() ~
+              c("{mean} ({sd})", "{median} [{min}, {max}]")
+          ),
+          # round mean to 1 digit, SD to 2 digits, Median and Range to 0
+          digits = list(`Age at visit` ~ c(1, 2, 0, 0, 0), CGG ~ c(1, 2, 0, 0, 0)),
+          missing_text = "Missing"
+        )
+    )
 
-    tbl_pval <- data |>
-      dplyr::select(all_of(vars), Gender) |>
-      gtsummary::tbl_summary(by = Gender, missing_text = "Missing") |>
-      gtsummary::add_p(
-        pvalue_fun = function(x)
-          gtsummary::style_number(x, digits = 3)
-      ) |>
-      gtsummary::modify_column_hide(columns = c(stat_1, stat_2)) |>
-      gtsummary::separate_p_footnotes()
+  tbl_pval <- data |>
+    dplyr::select(all_of(vars), Gender) |>
+    gtsummary::tbl_summary(by = Gender, missing_text = "Missing") |>
+    gtsummary::add_p(
+      # test = `Age at visit` ~ "add_p_test_wilcox.test2",
+      test.args =
+        list(
+        `Age at visit` ~
+        list(method = "p-value for significance of
+             sex difference by Wilcoxon rank sum test"),
+        `Primary Race/Ethnicity` ~
+        list(method = "p-value for significance of
+             sex difference by fisher rank sum test")
+        # gtsummary::all_tests("fisher.test") ~
+        #   list(method = "p-value for significance of sex difference by Fisher's exact test")
+        ),
+      pvalue_fun = function(x)
+        gtsummary::style_number(x, digits = 3)
+    ) |>
+    gtsummary::modify_column_hide(columns = c(stat_1, stat_2)) |>
+    gtsummary::separate_p_footnotes()
 
-    to_return = gtsummary::tbl_merge(list(tbl_stat, tbl_pval), tab_spanner = FALSE)
-    class(to_return) = c("demographics_table", class(to_return))
-    return(to_return)
+  to_return = list(tbl_stat, tbl_pval) |>
+    gtsummary::tbl_merge(tab_spanner = FALSE) |>
+    format_demographics_table_as_flextable()
 
-    }
+  return(to_return)
+
+}
