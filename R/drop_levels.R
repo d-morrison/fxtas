4e3ebc1b956a02b277ce951d7414656baeeb2201
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2024-10-31 22:32:07 -0700

    fixed labels in table_subtype_by_demographics()

--- R/drop_levels.R
+++ R/drop_levels.R
@@ -1,9 +1,9 @@
-drop_levels = function(x, except)
+drop_levels = function(x, except = NULL)
 {
   x |>
   mutate(
     across(
-      .cols = where(is.factor) & !except,
+      .cols = where(is.factor) & !all_of(except),
       .fns = forcats::fct_drop)
     # unlike droplevels(), fct_drop() preserves attributes
   )
