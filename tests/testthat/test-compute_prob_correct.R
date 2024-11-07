beb99e868acdde756c9c05e34daf8d0807550d11
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2024-11-06 17:22:00 -0800

    lint

--- tests/testthat/test-compute_prob_correct.R
+++ tests/testthat/test-compute_prob_correct.R
@@ -1,5 +1,4 @@
 test_that("compute_prob_correct() produces consistent results", {
-
   full_data = test_data_v1
 
   v1_usable = full_data |> dplyr::filter(CGG < 200)
@@ -21,43 +20,41 @@ test_that("compute_prob_correct() produces consistent results", {
     select(all_of(biomarker_varnames))
 
   control_data |>
-    compute_prob_correct(
-      max_prob = .95,
-      biomarker_levels = biomarker_levels) |>
+    compute_prob_correct(max_prob = .95, biomarker_levels = biomarker_levels) |>
     attr("data") |>
     ssdtools:::expect_snapshot_data(name = "prob-correct")
 
 })
 
-test_that("pander method for `prob_correct` objects produces consistent results", {
+test_that("pander method for `prob_correct` objects produces
+          consistent results",
+          {
+            pander::panderOptions("table.split.table", Inf)
 
-  pander::panderOptions("table.split.table", Inf)
+            full_data = trax_gp34_v1
 
-  full_data = trax_gp34_v1
+            v1_usable = full_data |> dplyr::filter(CGG < 200)
 
-  v1_usable = full_data |> dplyr::filter(CGG < 200)
+            biomarker_groups = compile_biomarker_groups_table()
 
-  biomarker_groups = compile_biomarker_groups_table()
+            biomarker_varnames =
+              biomarker_groups |>
+              pull("biomarker")
 
-  biomarker_varnames =
-    biomarker_groups |>
-    pull("biomarker")
+            biomarker_levels =
+              v1_usable |>
+              dplyr::select(all_of(biomarker_varnames)) |>
+              lapply(F = levels)
 
-  biomarker_levels =
-    v1_usable |>
-    dplyr::select(all_of(biomarker_varnames)) |>
-    lapply(F = levels)
+            control_data =
+              v1_usable |>
+              dplyr::filter(`FX*` == "CGG <55") |>
+              select(all_of(biomarker_varnames))
 
-  control_data =
-    v1_usable |>
-    dplyr::filter(`FX*` == "CGG <55") |>
-    select(all_of(biomarker_varnames))
+            control_data |>
+              compute_prob_correct(max_prob = .95,
+                                   biomarker_levels = biomarker_levels) |>
+              pander() |>
+              testthat::expect_snapshot()
 
-  control_data |>
-    compute_prob_correct(
-      max_prob = .95,
-      biomarker_levels = biomarker_levels) |>
-    pander() |>
-    testthat::expect_snapshot()
-
-})
+          })
