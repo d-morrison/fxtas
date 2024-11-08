99089ee077b28a462575a49cd4804518ce01f541
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2024-11-07 22:19:23 -0800

    new test achieved

--- tests/testthat/test-make_demographics_table.R
+++ tests/testthat/test-make_demographics_table.R
@@ -1,8 +1,15 @@
 test_that("results are consistent", {
-  test_data_v1 |>
+  ft = test_data_v1 |>
     dplyr::filter(!is.na(CGG)) |>
-    make_demographics_table() |>
-    expect_no_error()
+    make_demographics_table()
+
+  html_file <- tempfile(fileext = ".html")
+  flextable::save_as_html(ft, path = html_file)
+  doconv::expect_snapshot_html(
+    x = html_file,
+    name = "demographics_table",
+    engine = "testthat"
+  )
 })
 
 # need better tests here
