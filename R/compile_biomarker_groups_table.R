12e8d76ffeee578e9f892cc9bc1c6b793fd93c88
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2024-11-18 20:48:04 -0800

    fix

--- R/compile_biomarker_groups_table.R
+++ R/compile_biomarker_groups_table.R
@@ -5,12 +5,12 @@
 #' @returns a [tibble::tbl_df]
 #' @export
 #' @examples
-#' biomarker_groups_list = list(
+#' biomarker_group_list = list(
 #'   "group 1" = c("Biomarker 1", "Biomarker 2"),
 #'   "group 2" = c("Biomarker 3", "Biomarker 4"),
 #'   "group 3" = "Biomarker 5")
 #'
-#' biomarker_groups_list |> compile_biomarker_groups_table()
+#' compile_biomarker_groups_table(biomarker_group_list = biomarker_group_list)
 #'
 compile_biomarker_groups_table = function(
     dataset,
--- tests/testthat/test-make_biomarkers_table.R
+++ tests/testthat/test-make_biomarkers_table.R
@@ -1,14 +1,13 @@
 test_that("`test_biomarkers_table()` produces consistent results", {
 
-  biomarker_groups_list = list(
+  biomarker_group_list = list(
     "group 1" = c("Biomarker 1", "Biomarker 2"),
     "group 2" = c("Biomarker 3", "Biomarker 4"),
     "group 3" = "Biomarker 5"
   )
 
   biomarker_groups =
-    biomarker_groups_list |>
-    compile_biomarker_groups_table()
+    compile_biomarker_groups_table(biomarker_group_list = biomarker_group_list)
 
   SuStaInLabels = biomarkers = biomarker_varnames =
     biomarker_groups |>
