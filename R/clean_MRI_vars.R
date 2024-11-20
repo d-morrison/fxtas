cd03b2747d3d8599d9b42070ea57987203c4718e
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2024-11-19 23:44:25 -0800

    lint

--- R/clean_MRI_vars.R
+++ R/clean_MRI_vars.R
@@ -1,34 +1,26 @@
-clean_MRI_vars <- function(
-    dataset,
-    MRI_vars = c(
-      "Cerebral Atrophy",
-      "Cerebellar Atrophy",
-      "Cerebral WM Hyperintensity",
-      "Cerebellar WM Hyperintensity",
-      "MCP-WM Hyperintensity",
-      "Pons-WM Hyperintensity",
-      "Sub-Insular WM Hyperintensity",
-      "Periventricular WM Hyperintensity",
-      "Splenium (CC)-WM Hyperintensity",
-      "Genu (CC)-WM Hyperintensity",
-      "Corpus Callosum-Thickness"
-    ),
-    missing_codes = c(
-      "Missing/Refused (999)",
-      "999",
-      "Missing/Refused"))
-{
+clean_MRI_vars <- function(dataset,
+                           MRI_vars = c(
+                             "Cerebral Atrophy",
+                             "Cerebellar Atrophy",
+                             "Cerebral WM Hyperintensity",
+                             "Cerebellar WM Hyperintensity",
+                             "MCP-WM Hyperintensity",
+                             "Pons-WM Hyperintensity",
+                             "Sub-Insular WM Hyperintensity",
+                             "Periventricular WM Hyperintensity",
+                             "Splenium (CC)-WM Hyperintensity",
+                             "Genu (CC)-WM Hyperintensity",
+                             "Corpus Callosum-Thickness"
+                           ),
+                           missing_codes = c("Missing/Refused (999)",
+                                             "999",
+                                             "Missing/Refused")) {
   dataset |>
-    mutate(
-      across(
-        .cols = all_of(MRI_vars),
-        .fn <- function(x)
-        {
-          if_else(
-            x %in% missing_codes,
-            NA,
-            x) |>
-            droplevels()
-        })
-    )
+    mutate(across(
+      .cols = all_of(MRI_vars),
+      .fn = function(x) {
+        if_else(x %in% missing_codes, NA, x) |>
+          droplevels()
+      }
+    ))
 }
