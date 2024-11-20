c1f6f22b0082e6f286c4b55df870b395562791cc
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2024-11-19 23:44:25 -0800

    lint

--- R/categorize_SCL90.R
+++ R/categorize_SCL90.R
@@ -1,23 +1,17 @@
-categorize_SCL90 <- function(
-    dataset)
-{
-
-  to_return =
+categorize_SCL90 <- function(dataset) {
+  to_return <-
     dataset |>
-    mutate(
-      across(
-        contains("SCL", ignore.case = FALSE) &
-          !contains("missing"),
-        .fn <- function(x)
-          if_else(
-            x >= 60,
-            "60+",
-            "<60") |>
+    mutate(across(
+      contains("SCL", ignore.case = FALSE) &
+        !contains("missing"),
+      .fn = function(x) {
+        if_else(x >= 60, "60+", "<60") |>
           factor() |>
-          relevel(ref = "<60"),
-        .names = "{.col}*")
-    )
+          relevel(ref = "<60")
+      },
+      .names = "{.col}*"
+    ))
 
-  return(to_return)
+  to_return
 
 }
